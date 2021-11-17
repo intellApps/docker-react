c3036d8f89b66874609f343c3714a2695545dad4
unknown <intelligentappsofficial@gmail.com>
2021-11-17 12:15:56 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Making the first change</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>2nd change...</p>
+        <p>3rd change...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
